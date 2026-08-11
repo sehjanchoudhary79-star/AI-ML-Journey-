62149d8dcb6104e21a43d7d1b9b91984a13970fa
sehjanchoudhary79-star <sehjanchoudhary79@gmail.com>
2026-08-11 17:29:50 +0530

    logistic regression

---
+++ phase-3_machine_learning/logistic_regression.py
@@ -0,0 +1,51 @@
+import numpy as np 
+import pandas as pd\
+
+data=pd.read_csv("logistic_regression_dataset.csv")
+x=np.array(data .iloc[:,0:7])
+x=(x-x.mean(axis=0))/x.std(axis=0)
+y=np.array(data.iloc[:,-1])
+
+w=np.zeros(x.shape[1])
+b=0
+
+def logistic_regression(X,W,b):
+    z=np.dot(X,W)+b
+    f=1/(1+np.exp(-z))
+    return f
+
+def threshold(f):
+    return np.where(f>=0.5,1,0)
+    
+
+def gradient_descent(X,y,W,b,lr=0.00023,iteration=52000):
+
+    m=X.shape[0]
+
+    for i in range (iteration):
+        f=logistic_regression(X,W,b)
+        error=f-y
+        dw=1/m*np.dot(X.T,error)
+        db=1/m*np.sum(error)
+        W=W-lr*dw
+        b=b-lr*db
+
+    return W,b
+
+def accuracy(y,y_hat):
+    correct=np.sum(y==y_hat)
+    wrong=np.sum(y!=y_hat)
+    total=len(y)
+    acc=correct/total*100
+    print(f"correct:{correct}, wrong:{wrong}, total:{total}, accuracy:{acc}%")
+
+a,c=gradient_descent(x,y,w,b)
+print(a,c)  
+
+f=logistic_regression(x,a,c)
+print(f[:10])    
+
+y_hat=threshold(f)
+print(y_hat[:10])
+
+accuracy(y, y_hat)
